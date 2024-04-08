4ea3a6fe4156577ad9ba4ad3d2b3b1d8a2bd8e91
rszczykutowicz <r.szczykutowicz.extern@senec.com>
2024-04-08 13:39:21 +0200

    Initial maven commit.

---
+++ src/test/java/com/example/jsonscrapper/JsonplaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
